94419c58ba75e3ad42a85fd873347fc05787e685
jamesnunn <jamesnunn123@gmail.com>
2020-09-14 08:49:36 +0100

    Update q6.sql

--- q6.sql
+++ q6.sql
@@ -1,4 +1,4 @@
-- Maybe we're expecting a function here?
+- Maybe we're expecting a function here? In this case I'd expect the input as text and split the string and reform as WKT.
 
 select 
     ST_Intersects('POLYGON((10 11, 10 9, 11 9, 14 9, 14 10, 10 11))'::geometry, 'POINT(9 3)'::geometry) or 
